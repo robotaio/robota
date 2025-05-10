375675ed5d11b129dd8ceeda3413b659a7570153
Jungyoun <jungyoun@gmail.com>
2025-05-11 02:24:33 +0900

    Updates

--- scripts/redirect.js
+++ scripts/redirect.js
@@ -1,45 +1,40 @@
 
 // GitHub Pages에서 SPA 히스토리 모드를 지원하기 위한 스크립트
 (function() {
-  // 베이스 경로 구하기 (GitHub Pages의 경우 '/robota/' 형식)
-  var basePath = '';
-  if (window.location.hostname.indexOf('github.io') > -1) {
-    // GitHub Pages URL에서 리포지토리 이름 추출
-    var pathSegments = window.location.pathname.split('/');
-    if (pathSegments.length > 1) {
-      basePath = '/' + pathSegments[1] + '/';
-    }
-  }
-
   // 현재 URL에서 경로 부분 가져오기
   var path = window.location.pathname;
   var isGitHubPages = window.location.hostname.indexOf('github.io') > -1;
   
-  // 404 페이지에서 원래 URL로 리다이렉트 처리
-  if (path.endsWith('/404.html')) {
-    var redirectPath = sessionStorage.getItem('redirectPath');
-    if (redirectPath) {
-      sessionStorage.removeItem('redirectPath');
-      window.location.replace(redirectPath);
-    }
+  // 이미 index.html로 접근한 경우나 404 페이지인 경우는 처리하지 않음
+  if (path === '/' || path.endsWith('/index.html') || path.endsWith('/404.html')) {
     return;
   }
 
-  // GitHub Pages인 경우와 base path 이후 경로가 있는 경우에만 처리
-  if (isGitHubPages && path !== basePath && !path.endsWith('/index.html')) {
-    // HTML 파일로 직접 접근한 경우는 다르게 처리
-    if (path.endsWith('.html')) {
-      var htmlLessPath = path.replace('.html', '');
-      sessionStorage.setItem('redirectPath', htmlLessPath);
-      window.location.replace(basePath + '404.html');
-      return;
-    }
+  // HTML 파일로 직접 접근한 경우 처리
+  if (path.endsWith('.html')) {
+    window.location.href = path.replace('.html', '');
+    return;
+  }
 
-    // 현재 경로가 API 참조 경로인지 확인
-    if (path.includes('/api-reference/')) {
-      // 현재 경로 저장 후 404.html로 리다이렉트
+  // 디렉토리 경로에 후행 슬래시가 없는 경우 추가
+  if (!path.endsWith('/') && !path.includes('.')) {
+    window.location.href = path + '/';
+    return;
+  }
+
+  // GitHub Pages의 경우 SPA 리다이렉트 처리
+  if (isGitHubPages) {
+    // 404.html 페이지에서 원래 경로를 복원
+    if (path === '/404.html') {
+      var redirectTo = sessionStorage.getItem('redirectPath');
+      if (redirectTo) {
+        sessionStorage.removeItem('redirectPath');
+        window.location.replace(redirectTo);
+      }
+    } else {
+      // 현재 경로를 저장하고 404.html로 리다이렉트
       sessionStorage.setItem('redirectPath', path);
-      window.location.replace(basePath + '404.html');
+      window.location.replace('/404.html');
     }
   }
 })();
